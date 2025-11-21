3cbfc3896c655d36105113a203d257f6bd18f61c
gustavbagger <gustavbagger@me.com>
2025-11-21 12:58:29 +1100

    cliCommand struct, missing: defer os.Exit() with correct exit text

--- commands/commands.go
+++ commands/commands.go
@@ -5,20 +5,6 @@ import (
 	"os"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-var Supported = map[string]cliCommand{
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    CExit,
-	},
-}
-
 func CExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
--- main.go
+++ main.go
@@ -5,23 +5,37 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/gustavbagger/Pokedex/helpers"
+	"github.com/gustavbagger/Pokedex/commands"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var Supported = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commands.CExit,
+	},
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		text := scanner.Text()
-		cleaned := helpers.CleanInput(text)
-		if len(cleaned) == 0 {
-			fmt.Println("Your command was: ")
+		command := scanner.Text()
+		if cli, ok := Supported[command]; ok {
+			err := cli.callback
+			if err != nil {
+				fmt.Errorf("callback error: %v", err)
+				return
+			}
 		} else {
-			fmt.Printf(
-				"Your command was: %s\n",
-				cleaned[0],
-			)
+			fmt.Print("Unknown command\n")
 		}
 	}
 }
